05e5747e6820bc6fda61e1625022ee4730f7f812
EmaEliasDv <emanuel.elias.davalos@gmail.com>
2024-02-27 23:28:47 -0300

    E1

---
+++ main.js
@@ -0,0 +1,123 @@
+// 1- Crear una función que reciba un número por parámetro e indique en consola si el número es par o impar.
+// const numero = 5;
+
+// function parImpar(numero){
+//     if(numero%2===0){
+//         console.log("El número es par");
+//     }else{
+//         console.log("El número es impar");
+//     }
+// }
+// parImpar(numero);
+
+//Imprime que el número es impar.
+
+//---------------------------------------------------------------------------------------------------------
+
+// 2- Crear una función que reciba dos números por parámetro e indique en consola que número es mayor, y si ninguno lo es, indicar por consola que son iguales.
+// const numero1 = 9;
+// const numero2 = 11;
+
+// function mayorMenor(numero1, numero2){
+//     if(numero1>numero2){
+//         console.log("El número 1 es mayor que el número 2");
+//     }else if(numero2>numero1){
+//         console.log("El número 2 es mayor que el número 1");
+//     }else{
+//         console.log("Los números son iguales");
+//     }
+// }
+// mayorMenor(numero1, numero2);
+
+// Imprime que el número 2 es mayor que el número 1.
+
+//---------------------------------------------------------------------------------------------------------
+
+// 3- Crear una función que reciba un número por parámetro e indique en consola si ese número es múltiplo de 5.
+// const numero = 10;
+
+// function multiploDe5(numero){
+//     if(numero%5===0){
+//         console.log("El número es múltiplo de 5");
+//     }else{
+//         console.log("El número no es múltiplo de 5");
+//     }
+// }
+// multiploDe5(numero);
+
+// Imprime que el número es múltiplo de 5.
+
+//---------------------------------------------------------------------------------------------------------
+
+// 4- Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número.
+// const numero = 10;
+
+// function imprimirNumeros(numero){
+//     for(let i=0; i<=numero; i++){
+//         console.log(i);
+//     }
+// }
+// imprimirNumeros(numero);
+
+// Imprime del 0 al 10.
+
+//---------------------------------------------------------------------------------------------------------
+
+// 5 - Crear una función que reciba una palabra y un número por parámetro e imprima por consola  esa palabra la cantidad correspondiente al número indicado.
+// const palabra = "Hola";
+// const numero = 5;
+
+// function imprimirPalabra(palabra, numero){
+//     for(let i=0; i<numero; i++){
+//         console.log(palabra);
+//     }
+// }
+// imprimirPalabra(palabra, numero);
+
+//Imprime Hola 5 veces.
+
+//---------------------------------------------------------------------------------------------------------
+
+// 6 - Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array.
+// const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+// function imprimirArray(array){
+//     for(let i=0; i<array.length; i++){
+//         console.log(array[i]);
+//     }
+// }
+// imprimirArray(array);
+
+//Imprime todos los valores del array.
+
+//---------------------------------------------------------------------------------------------------------
+
+// 7 - Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo. Ayuda: Recuerden que el primer índice de un array es "0".
+// const array = [1,2,3,4,5,6,7,8,9,10];
+
+// function imprimirArray(array){
+//     for(let i=0; i<array.length; i++){
+//         if(i===4){
+//             continue;
+//         }
+//         console.log(array[i]);
+//     }
+// }
+// imprimirArray(array);
+
+//Imprime todos los valores del array menos el que se encuentra en la 5ta posición.
+
+//---------------------------------------------------------------------------------------------------------
+
+// 8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
+// const array = [1,2,3,4,5];
+
+// function multiplicarArray(array, numero){
+//     for(let i=0; i<array.length; i++){
+//         console.log(array[i]*numero);
+//     }
+// }
+
+// multiplicarArray(array, 5);
+
+//Imprime cada número del array multiplicado por 5.
