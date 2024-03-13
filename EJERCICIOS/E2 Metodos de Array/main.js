dab1665a583b4cf7f330420798c81b8e495ab3ce
EmaEliasDv <emanuel.elias.davalos@gmail.com>
2024-03-12 22:30:04 -0300

    Se realizó el Ejercicio de 2

---
+++ EJERCICIOS/E2 Metodos de Array/main.js
@@ -0,0 +1,95 @@
+/* Utilizando métodos de array e iterando sobre el
+array de pizzas, realizar las siguientes actividades,
+imprimiendo en consola: */
+
+// a) Las pizzas que tengan un id impar.
+// b) Responder: ¿Hay alguna pizza que valga menos de $600?
+// c) El nombre de cada pizza con su respectivo precio.
+/* d) Todos los ingredientes de cada pizza (En cada iteración
+imprimir los ingredientes de la pizza que se esta recorriendo).
+Ayuda: van a tener que realizar dos recorridos, ya que cada pizza del array de
+pizzas tiene una propiedad "ingredientes" cuyo valor es un array con
+ingredientes. */
+
+const pizzas = [
+    {
+        id: 1,
+        nombre: "pizza de Muzzarella",
+        precio: 500,
+        ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+    },
+
+    {
+        id: 2,
+        nombre: "pizza de Cebolla",
+        precio: 1500,
+        ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+    },
+
+    {
+        id: 3,
+        nombre: "pizza Napolitana",
+        precio: 1350,
+        ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas"],
+    },
+
+    {
+        id: 4,
+        nombre: "pizza 4 Quesos",
+        precio: 1380,
+        ingredientes: [
+            "Muzzarella",
+            "Tomate",
+            "Queso Azul",
+            "Parmesano",
+            "Roquefort",
+        ],
+    },
+
+    {
+        id: 5,
+        nombre: "pizza Especial",
+        precio: 1000,
+        ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas", "Cebolla"],
+    },
+
+    {
+        id: 6,
+        nombre: "pizza con Anana",
+        precio: 600,
+        ingredientes: ["Muzzarella", "Tomate", "Anana"],
+    },
+];
+
+// -------------------- a) Las pizzas que tengan un id impar.
+
+// const pizzasImpares = pizzas.filter((pizza) => pizza.id % 2 !== 0);
+// console.log("Las pizzas que contienen un ID impar son: ", pizzasImpares.map((pizza) => pizza.nombre));
+
+
+// -------------------- b) Responder: ¿Hay alguna pizza que valga menos de $600?
+
+// const pizzaBarata = pizzas.some((pizza) => pizza.precio < 600);
+// if (pizzaBarata) {
+//     console.log("Si hay una pizza que vale menos de $600 y es: ", pizzas.find((pizza) => pizza.precio < 600).nombre, "y su precio es de: ", pizzas.find((pizza) => pizza.precio < 600).precio);
+// } else {
+//     console.log("No hay una pizza que valga menos de $600");
+// }
+
+
+// -------------------- c) El nombre de cada pizza con su respectivo precio.
+
+// pizzas.forEach((pizza) => {
+//     console.log(`La pizza ${pizza.nombre} tiene un precio de $${pizza.precio}`);
+// });
+
+
+// -------------------- d) Todos los ingredientes de cada pizza (En cada iteración imprimir los ingredientes de la pizza que se esta recorriendo).
+
+// pizzas.forEach((pizza) => {
+//     console.log(`Los ingredientes de la pizza ${pizza.nombre} son: ${pizza.ingredientes}`);
+// });
+
+
+
+
